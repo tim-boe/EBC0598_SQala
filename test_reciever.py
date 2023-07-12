8f37cac6d28fa72b2bf96599a80958a28b4e60e8
Jun Jiang <admin-jji@ESIM100.eonerc.rwth-aachen.de>
2023-07-12 14:43:18 +0200

    cleand up test_reciever.py Signed-off-by: Jun Jiang <admin-jji@ESIM100.eonerc.rwth-aachen.de>

--- test_reciever.py
+++ test_reciever.py
@@ -1,93 +1,9 @@
 import argparse
-import math
 import pyads
-
 from pythonosc.dispatcher import Dispatcher
-from pythonosc.osc_server import AsyncIOOSCUDPServer
 from pythonosc import osc_server
-import asyncio
-import time
-import datetime
 
 
-# dispatcher = Dispatcher()
-# dispatcher.map("/filter", filter_handler)
-
-# ip = "127.0.0.1"
-# port = 55555
-#
-#
-# class MyAds:
-#     def __init__(self, symbol_list_file, ams_net_id, ams_net_port=851, logger=False, ctrl_sched_file=None):
-#         self.symbol_list_file = symbol_list_file
-#         self.symbol_list_file_autogen = 'AdsSymList_AutoGen.txt'
-#         self.ams_net_id = ams_net_id
-#         self.ams_net_port = ams_net_port
-#         self.plc = pyads.Connection(ams_net_id, ams_net_port)
-#         self.plc.open()
-#         self.state = self.plc.read_state()
-#         print(f"PLC read state: {self.state}")
-#
-#     def write(self, name, value, plc_type):
-#         if plc_type == "BOOL":
-#             plc_type = pyads.PLCTYPE_BOOL
-#         elif plc_type == "REAL":
-#             plc_type = pyads.PLCTYPE_REAL
-#         elif plc_type == "INT":
-#             plc_type = pyads.PLCTYPE_INT
-#         elif plc_type == "DT":
-#             plc_type = pyads.PLCTYPE_DT
-#         elif plc_type == "STRING":
-#             plc_type = pyads.PLCTYPE_STRING
-#         elif plc_type == "DWORD":
-#             plc_type = pyads.PLCTYPE_DWORD
-#         # write to the plc
-#         self.plc.write_by_name(name, value, plc_type)
-#
-#
-# # def print_volume_handler(unused_addr, args, volume):
-# #     print("[{0}] ~ {1}".format(args[0], volume))
-# #
-# #
-# # def print_compute_handler(unused_addr, args, volume):
-# #     try:
-# #         print("[{0}] ~ {1}".format(args[0], args[1](volume)))
-# #     except ValueError:
-# #         pass
-#
-#
-# # async def loop():
-# #     """Main ADS loop"""
-# #     try:
-# #         myPlc = MyAds(symbol_list_file='01_LogVarList.txt', ams_net_id='5.78.127.222.1.1', ams_net_port=851,
-# #                       logger=False,
-# #                       ctrl_sched_file=None)  # Set ctrl_sched_file to None to deactivate the function
-# #         # while True:
-# #         #     t1 = time.time()
-# #         #     myPlc.run()
-# #         #     # apply the interval
-# #         #     t2 = time.time()
-# #         #     delta_t = t2 - t1
-# #         #     print('delta_t = ' + str(delta_t))
-# #     except KeyboardInterrupt:
-# #         myPlc.plc.close()
-# #         # myPlc.plc.release_handle(myPlc.var_handle)
-# #         print('\nPLC closed, handle released.')
-#
-#
-# # async def init_main():
-# #     server = AsyncIOOSCUDPServer((ip, port), dispatcher, asyncio.get_event_loop())
-# #     transport, protocol = await server.create_serve_endpoint()  # Create datagram endpoint and start serving
-# #
-# #     await loop()  # Enter main loop of program
-# #
-# #     transport.close()  # Clean up serve endpoint
-#
-# def filter_handler(address, *args):
-#     print(f"{address}: {args}")
-#
-# def handle_bool(ads):
-#     ads.write()
 def osc_handler(address, *args):
     # Write to the PLC global variable
     value = args[0]  # Assuming a single value is sent in the OSC message
